8592675ba852d1b48680945fb77971992cef9eb5
Jun Zheng <me@jackzh.com>
2019-01-15 02:06:41 -0500

    fix: Increase upload limit

--- middlewares/parsing/index.js
+++ middlewares/parsing/index.js
@@ -14,5 +14,5 @@ const bodyParser = require('body-parser');
  */
 module.exports = function(app) {
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}));
 };
